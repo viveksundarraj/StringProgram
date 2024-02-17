a9dbfc3634b69be1a2e076e71075cc78fbce2342
VivekSundarraj1996 <viveksundarraj99@gmail.com>
2024-02-18 01:43:21 +0530

    Array committed

---
+++ ArrayProgram.java
@@ -0,0 +1,14 @@
+class ArrayProgram{
+       public static void main(String args[])
+      {
+
+          int arr[]={1,2,3,4};
+
+           System.out.println(Arrays.toString(arr));
+
+
+     }
+
+
+
+}
